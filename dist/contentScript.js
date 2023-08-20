eeb4da559a5d4382bfd785fd9b3c0ce9817ebe58
Dun-sin <jesudunsinfaiye@gmail.com>
2023-08-20 20:05:14 +0100

    feat: made content script to work

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -1,91 +1,23 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/content/contentScript.ts":
+var __webpack_exports__ = {};
 /*!**************************************!*\
   !*** ./src/content/contentScript.ts ***!
   \**************************************/
-/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+window.onload = () => {
+    // Check if we're on a GitHub repository page
+    const isRepo = window.location.href.match('https://github.com/[A-Za-z0-9_-]+/[A-Za-z0-9_-]+(/.*)?');
+    if (isRepo !== null) {
+        const button = document.createElement('button');
+        button.innerText = 'My Button';
+        document.body.appendChild(button);
+        // Add an event handler to the button
+        button.addEventListener('click', () => {
+            // Perform some action when the button is clicked
+            alert('Button Clicked!');
+        });
+    }
 };
-(() => __awaiter(this, void 0, void 0, function* () {
-    const src = chrome.runtime.getURL('./content_main.js');
-    const contentScript = yield __webpack_require__("./src/content lazy recursive")(src);
-    yield contentScript.main();
-}))();
 
-
-/***/ }),
-
-/***/ "./src/content lazy recursive":
-/*!********************************************!*\
-  !*** ./src/content/ lazy namespace object ***!
-  \********************************************/
-/***/ ((module) => {
-
-function webpackEmptyAsyncContext(req) {
-	// Here Promise.resolve().then() is used instead of new Promise() to prevent
-	// uncaught exception popping up in devtools
-	return Promise.resolve().then(() => {
-		var e = new Error("Cannot find module '" + req + "'");
-		e.code = 'MODULE_NOT_FOUND';
-		throw e;
-	});
-}
-webpackEmptyAsyncContext.keys = () => ([]);
-webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
-webpackEmptyAsyncContext.id = "./src/content lazy recursive";
-module.exports = webpackEmptyAsyncContext;
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/content/contentScript.ts");
-/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=contentScript.js.map
