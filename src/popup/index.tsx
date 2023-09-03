5eb0f09a50f1d123b10d7c922ae8f08497c5c90a
Dunsin <jesudunsinfaiye@gmail.com>
2023-09-03 19:06:42 +0100

    feat(content-script): add nessacry structure for content script
    fix #2

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Popup from './Popup';
+import Popup from './components/Popup';
+
+import '../assets/css/tailwind.css';
 
 const Index = () => <Popup />;
 
