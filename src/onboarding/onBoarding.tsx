8cc66c344853c0bbc1848d99cdd3a4cfce3fa032
Dun-sin <jesudunsinfaiye@gmail.com>
2023-11-12 12:09:19 +0100

    fix: onBoarding error

--- src/onboarding/onBoarding.tsx
+++ src/onboarding/onBoarding.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import '../assets/css/tailwind.css';
+import Button from 'components/Button';
 
 const OnBoarding: React.FC = () => {
-    return (
-        <div className="h-screen w-screen bg-blue-900 flex justify-center items-center flex-col">
-            <h1 className=" text-4xl text-white">Welcome to Open Source Pal</h1>
-            <p>Make Contributing to open source easier</p>
-        </div>
-    );
+	return (
+		<div className="h-screen w-screen bg-primary flex justify-center items-center flex-col text-white">
+			<div>
+				<h1 className="text-4xl">Welcome to Open Source Pal</h1>
+				<p>Make Contributing to open source easier</p>
+			</div>
+		</div>
+	);
 };
 
 export default OnBoarding;
