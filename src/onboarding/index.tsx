877756ee52556cdf1f52d2aaa5b068d83160b4e2
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-11-12 19:40:02 +0100

    docs: Delete src/onboarding directory

--- src/onboarding/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import OnBoarding from './OnBoarding';
-
-const Index = () => <OnBoarding />;
-
-const root = createRoot(document.getElementById('display-container')!);
-root.render(<Index />);
