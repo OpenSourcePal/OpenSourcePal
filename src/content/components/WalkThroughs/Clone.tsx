e6358c18d3c96320e5400503bd3ef9fd526be116
Dun-sin <jesudunsinfaiye@gmail.com>
2023-12-19 11:24:06 +0100

    fix(walkthrough): show me button not working

--- src/content/components/WalkThroughs/Clone.tsx
+++ src/content/components/WalkThroughs/Clone.tsx
@@ -5,19 +5,19 @@ import Code from 'components/Code';
 
 import { useArrowNavigation } from 'content/hooks/useArrow';
 
-const Clone = () => {
-	const { onClickWalkThrough } = useArrowNavigation();
+import { isFirefox } from 'content/utils/lib';
 
+const Clone = () => {
 	return (
 		<>
 			<ul className="px-3 py-2 flex flex-col gap-2 bg-secondary text-primary">
 				<li className="flex items-center justify-between">
 					<span>Open the "Code" dropdown</span>
-					<Button label="Show Me" action={() => onClickWalkThrough('.js-codespaces-details-container')} className="px-2 py-1 rounded-sm bg-primary text-white" />
+					<ShowMe cssClass=".js-codespaces-details-container" />
 				</li>
 				<li className="flex items-center justify-between">
 					<span>Copy the URL</span>
-					<Button label="Show Me" action={() => onClickWalkThrough('.js-clone-url-http')} className="px-2 py-1 rounded-sm bg-primary text-white" />
+					<ShowMe cssClass=".js-clone-url-http" />
 				</li>
 				<li>
 					<p>Paste the URL in your terminal</p>
@@ -29,3 +29,8 @@ const Clone = () => {
 };
 
 export default Clone;
+
+const ShowMe = ({ cssClass }: { cssClass: string }) => {
+	const { onClickWalkThrough } = useArrowNavigation();
+	return <Button label="Show Me" action={() => onClickWalkThrough('.js-clone-url-http')} className={`px-2 py-1 rounded-sm bg-primary text-white ${!isFirefox && 'hidden'}`} />;
+};
---
+++ src/content/utils/lib.ts
@@ -0,0 +1,1 @@
+export const isFirefox = navigator.userAgent.match(/firefox|fxios/i);
